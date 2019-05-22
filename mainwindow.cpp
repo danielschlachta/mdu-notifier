68beea2df5e7885ac9305e7aa05cfe09c87762f1
Daniel Schlachta <daniel.schlachta@gmail.com>
2019-05-22 19:25:16 +0000

    Forgot to check for show interval == 0

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -131,6 +131,7 @@ void MainWindow::paintTrayIcon()
     if (active && capBytes > 0
             && (capBytes - usedBytes) * 100 < capBytes * warn
             && settings->value("captime", 0).toInt() < capTime
+            && show > 0
             && (messageShown.elapsed() == 0 || messageShown.elapsed() >= show * 1000 * 60))
     {
         QString msg;
